a478d459744973ef18d51053a472c2e94c5d6d64
Natanael Acero <nataacero1@gmail.com>
2021-10-03 23:12:51 -0500

    REF-4 Se realizó el componente principal de cajónes y se preparó el entorno de trabajo

---
+++ src/components/CajonGrid.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const CajonGrid = (cajon) => {
+    return (
+        <div>
+            {cajon}
+        </div>
+    )
+}
---
+++ src/components/CajonId.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react'
+import { CajonGrid } from './CajonGrid'
+
+
+export const CajonId = () => {
+    const [cajones, setCajones] = useState([])
+
+    const getCajonesById = (id) => {
+        setCajones(cajon => [
+            ...cajon, id
+        ]);
+    }
+    useEffect(() => {
+        getCajonesById('1');
+    }, [])
+
+    return (
+        <>
+            <h2>Cajones</h2>s
+            <ol>
+                {
+                    cajones.map(cajon => { <CajonGrid key={cajon} cajon={cajon} /> })
+                }
+            </ol>
+        </>
+    )
+}
